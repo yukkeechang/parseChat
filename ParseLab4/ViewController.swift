df2b123610333db8bedb24f6d77bf4e1b756f253
YukkeeChang <yukkeechang@gmail.com>
2017-02-22 15:43:13 -0800

    Initial Commit

---
+++ ParseLab4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseLab4
+//
+//  Created by Yukkee chang on 2/22/17.
+//  Copyright © 2017 Yukkee chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
