cc617ebd240ca439e9e2cc75b104cf50e33d72d7
YukkeeChang <yukkeechang@gmail.com>
2017-02-27 15:54:16 -0800

    two bugs

--- ParseLab4/loginViewController.swift
+++ ParseLab4/loginViewController.swift
@@ -11,48 +11,20 @@ import Parse
 
 class loginViewController: UIViewController  {
     @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var signUpButton: UIButton!
-    @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
-       
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func myMethod () {
-        var user = PFUser()
-        user.username = usernameTextField.text
-        user.password = passwordTextField.text 
-        
-      
-        user.signUpInBackground () {
-            (succeeded: Bool?, error: Error?) -> Void in
-            if error == nil {
-                let errorString = ["error"]
-                print("error with signup")
-            } else {
-                self.performSegue(withIdentifier: "createNewUserAndGoToDashboard", sender: self)
-                print("sucess")
-            }
-            
-            var currentUser = PFUser.current()
-            if currentUser != nil {
-                            } else {
-                // Show the signup or login screen
-                //put the error or alert thingie here
-            }
-        }
-        
+    
+    @IBAction func loginButton(_ sender: Any) {
         PFUser.logInWithUsername(inBackground: "usernameTextField", password: "passwordTextField") {
             (user: PFUser?, error: Error?) -> Void in
             if let error = error {
@@ -61,14 +33,38 @@ class loginViewController: UIViewController  {
             } else {
                 print("Logged in!");
             }
+            
         }
         
-        
-        
+       // @IBAction
+        func signupButton(_ sender: Any) {
+            var user = PFUser()
+            user.username = usernameTextField.text
+            user.password = passwordTextField.text
+            
+            user.signUpInBackground () {
+                (succeeded: Bool?, error: Error?) -> Void in
+                if error == nil {
+                    let errorString = ["error"]
+                    print("error with signup")
+                } else {
+                    self.performSegue(withIdentifier: "createNewUserAndGoToDashboard", sender: self)
+                    print("sucess")
+                }
+            }
+        }
+    /*    var currentUser = PFUser.current()
+        if currentUser != nil {
+        } else {
+            // Show the signup or login screen
+            //put the error or alert thing here
+        } */
+    }
     
-     
-        
-        
-
-   }
+    
+    func loginSegue() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
+        self.present(vc, animated: true, completion: nil)
+    }
 }
